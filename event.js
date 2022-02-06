412f2660469782f9c691b0758eaa8710d04935a2
31b4 <75566095+31b4@users.noreply.github.com>
2022-02-06 19:20:51 +0100

    Add files via upload

--- event.js
+++ event.js
@@ -1,9 +1,18 @@
+var sor
+var oszlop
+$(document).contextmenu(function() {
+    return false;
+});
+function RestartGame(){
+    $("#keret").html("");
+}
 $(document).ready(function() {
     $("input[type=button]").click(function(){
+        var start = [-1,-1]
         $("#keret").html("");
         let t=document.createElement("table");
-        let sor=$("#sor").val();
-        let oszlop=$("input[name=oszlop]").val();
+        sor=$("#sor").val();
+        oszlop=$("input[name=oszlop]").val();
         let aknak=$("#akna").val();
         if(aknak>sor*oszlop-1){
             alert("nana")
@@ -50,6 +59,7 @@ $(document).ready(function() {
         for (let i = 0; i < sor; i++) {
             for (let j = 0; j < oszlop; j++) {
                 if($("tr").eq(i).find("td").eq(j).text()=="_") {
+                    start = [i,j]
                     let db= 0
                     let nb = [[-1,-1],[-1,0],[-1,1],[1,-1],[1,0],[1,1],[0,-1],[0,1]]
                     for (let d = 0; d < nb.length; d++) {
@@ -59,28 +69,82 @@ $(document).ready(function() {
                     }
                     $("tr").eq(i).find("td").eq(j).text(db)
                     $("tr").eq(i).find("td").eq(j).click(function(){
-                        $("tr").eq(i).find("td").eq(j).css("background-color","green")
+                        
+
+                        Rekurzio(this.parentNode.rowIndex,this.cellIndex)
                     })
                 }else{
-                    $("tr").eq(i).find("td").eq(j).click(function(){
-                        $("tr").eq(i).find("td").eq(j).css("background-color","red")
+                    $("tr").eq(i).find("td").eq(j).click(function(){//endgame
+                                alert("buktad")
+                                RestartGame()
                     })
+                    $("tr").eq(i).find("td").eq(j).contextmenu(function(){//endgame
+
+                        $("tr").eq(this.parentNode.rowIndex).find("td").eq(this.cellIndex).css("background-color","red")
+                   
+                     
+                
+
+                    })
+            
                 }
                 
             }
         }
-        $("#td").click(function(){
-            if (this.text=="X") {
-                this.css("background-color","red")
-            }else{
-                this.css("background-color","green")
-            }
-        })
+        // $("#td").click(function(){
+        //     if (this.text=="X") {
+        //         this.css("background-color","red")
+        //     }else{
+        //        // alert('My position in table is: '+this.cellIndex+'x'+this.parentNode.rowIndex);
+        //         Rekurzio(this.cellIndex,this.parentNode.rowIndex)
+        //         //this.css("background-color","green")
+        //     }
+        // })
         //$("td").css("background-color","green");
         //$("td:contains('X')").css("background-color","red");
      
-        
+        //Rekurzio(start[0],start[1])
     });
 });
 
-//hf uresmezok körülötte hány db bomba van, írjuk bele
+
+//rekurziv korbejaras-------------------
+function Rekurzio(x,y){
+    //console.log(x,y)
+    //your code to be executed after 1 second 
+    //setTimeout(function() { 
+    //}, 200);
+    if ($("tr").eq(x).find("td").eq(y).text()!="0" && $("tr").eq(x).find("td").eq(y).text()!="X") {
+        $("tr").eq(x).find("td").eq(y).css("background-color","black")
+
+    } 
+    if( $("tr").eq(x).find("td").eq(y).text()=="0" ){
+        
+        $("tr").eq(x).find("td").eq(y).css("background-color","black")
+        $("tr").eq(x).find("td").eq(y).text("-")
+        if (y+1<oszlop) {
+            Rekurzio(x,y+1)
+        }
+        if (y-1>=0) {
+            Rekurzio(x,y-1)
+        }
+        if (x+1<sor) {
+            
+            Rekurzio(x+1,y)
+        }
+        if (x-1>=0) {
+            Rekurzio(x-1,y)
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
